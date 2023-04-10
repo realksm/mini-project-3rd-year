5fb1603481927e9e913fc6e8c66d11f3aa30ffc2
K Sunder Mishra <73066899+realksm@users.noreply.github.com>
2023-04-10 21:49:23 +0530

    Update insta.py

--- Model/insta.py
+++ Model/insta.py
@@ -1,94 +1,94 @@
-# #importing libraries
-# import pandas as pd
-# from pandas.io.json import json_normalize
-# from InstagramAPI import InstagramAPI
-# import time
+#importing libraries
+import pandas as pd
+from pandas.io.json import json_normalize
+from InstagramAPI import InstagramAPI
+import time
 
-# #LOGIN
-# def login_to_instagram(username, password):
-#     api = InstagramAPI(username, password)
-#     api.login()
-#     return api
+#LOGIN
+def login_to_instagram(username, password):
+    api = InstagramAPI(username, password)
+    api.login()
+    return api
 
-# api = login_to_instagram('enter_your_username_here','enter_your_password_here')
+api = login_to_instagram('enter_your_username_here','enter_your_password_here')
 
-# #Retriving Posts
-# def get_my_posts(api):
-#     '''Retrieve all posts from own profile'''
-#     my_posts = []
-#     has_more_posts = True
-#     max_id= ''
+#Retriving Posts
+def get_my_posts(api):
+    '''Retrieve all posts from own profile'''
+    my_posts = []
+    has_more_posts = True
+    max_id= ''
 
-#     while has_more_posts:
-#         api.getSelfUserFeed(maxid=max_id)
-#         if api.LastJson['more_available'] is not True:
-#             has_more_posts = False #stop condition
+    while has_more_posts:
+        api.getSelfUserFeed(maxid=max_id)
+        if api.LastJson['more_available'] is not True:
+            has_more_posts = False #stop condition
 
-#         max_id = api.LastJson.get('next_max_id','')
-#         my_posts.extend(api.LastJson['items']) #merge lists
-#         time.sleep(2) # slows down to avoid flooding
+        max_id = api.LastJson.get('next_max_id','')
+        my_posts.extend(api.LastJson['items']) #merge lists
+        time.sleep(2) # slows down to avoid flooding
 
-#         if has_more_posts:
-#             print(str(len(my_posts)) + ' posts retrieved so far...')
+        if has_more_posts:
+            print(str(len(my_posts)) + ' posts retrieved so far...')
 
-#     print('Total posts retrieved: ' + str(len(my_posts)))
+    print('Total posts retrieved: ' + str(len(my_posts)))
     
-#     return my_posts
-#     len(my_posts)
-#     my_posts[2]
+    return my_posts
+    len(my_posts)
+    my_posts[2]
 
-# my_posts = get_my_posts(api)
+my_posts = get_my_posts(api)
 
-# #Retrive Post Likers
-# def get_posts_likers(api, my_posts):
-#     '''Retrieve all likers on all posts'''
+#Retrive Post Likers
+def get_posts_likers(api, my_posts):
+    '''Retrieve all likers on all posts'''
     
-#     likers = []
+    likers = []
     
-#     print('wait %.1f minutes' % (len(my_posts)*2/60.))
-#     for i in range(len(my_posts)):
-#         m_id = my_posts[i]['id']
-#         api.getMediaLikers(m_id)
+    print('wait %.1f minutes' % (len(my_posts)*2/60.))
+    for i in range(len(my_posts)):
+        m_id = my_posts[i]['id']
+        api.getMediaLikers(m_id)
         
-#         likers += [api.LastJson]
+        likers += [api.LastJson]
         
-#         # Include post_id in likers dict list
-#         likers[i]['post_id'] = m_id
+        # Include post_id in likers dict list
+        likers[i]['post_id'] = m_id
         
-#         time.sleep(2)
-#     print('done')
+        time.sleep(2)
+    print('done')
     
-#     return likers
+    return likers
 
 
-# likers = get_posts_likers(api, my_posts)
+likers = get_posts_likers(api, my_posts)
 
 
-# #Likers to pandas df
-# def posts_likers_to_df(likers):
-#     '''Transforms likers list of dicts into pandas DataFrame'''
+#Likers to pandas df
+def posts_likers_to_df(likers):
+    '''Transforms likers list of dicts into pandas DataFrame'''
     
-#     # Normalize likers by getting the 'users' list and the post_id of each like
-#     df_likers = json_normalize(likers, 'users', ['post_id'])
+    # Normalize likers by getting the 'users' list and the post_id of each like
+    df_likers = json_normalize(likers, 'users', ['post_id'])
     
-#     # Add 'content_type' column to know the rows are likes
-#     df_likers['content_type'] = 'like'
+    # Add 'content_type' column to know the rows are likes
+    df_likers['content_type'] = 'like'
     
-#     return df_likers
+    return df_likers
 
-# df_likers = posts_likers_to_df(likers)
+df_likers = posts_likers_to_df(likers)
 
-# print('Total posts: ' + str(len(my_posts)))
-# print('---------')
-# print('Total likes on profile: ' + str(df_likers.shape[0])) #shape[0] represents number of rows
-# print('Distinct users that liked your posts: ' +str(df_likers.username.nunique())) # nunique() will count distinct values of a col
-# print('---------')
+print('Total posts: ' + str(len(my_posts)))
+print('---------')
+print('Total likes on profile: ' + str(df_likers.shape[0])) #shape[0] represents number of rows
+print('Distinct users that liked your posts: ' +str(df_likers.username.nunique())) # nunique() will count distinct values of a col
+print('---------')
 
 
-# #Top 30 likers
-# df_likers.username.value_counts()[:30]
-# df_likers.username.value_counts()[:30].plot(kind='bar', title='Top 30 media likers', grid=True, figsize=(12,6))
-# df_likers.username.value_counts()[:30].plot(kind='pie', title='Top 30 media likers distribution', autopct='%1.1f%%', figsize=(12,6))
+#Top 30 likers
+df_likers.username.value_counts()[:30]
+df_likers.username.value_counts()[:30].plot(kind='bar', title='Top 30 media likers', grid=True, figsize=(12,6))
+df_likers.username.value_counts()[:30].plot(kind='pie', title='Top 30 media likers distribution', autopct='%1.1f%%', figsize=(12,6))
 
 
 
